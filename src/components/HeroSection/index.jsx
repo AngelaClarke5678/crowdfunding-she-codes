987557c5d730c6c89cf8d628767435f3c0634b39
Angela Clarke <angeladoust@hotmail.com>
2022-12-07 09:05:32 +1000

    text updates

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Logo from '../img/Contributi-Large.png'
 import './HeroSection.css'
 
-function HeroSection () {
-    return <div className='container'>
-      <div className='herotext'><h1>Build a better community.</h1></div><div className="heroimage"><img src={ Logo } alt="Logo" /></div>
-      </div>;
-  }
-  
-  export default HeroSection;
+function HeroSection() {
+  return <div className='container'>
+    <div className='herotext'><h1>Build a better Community.</h1></div><div className="heroimage"><img src={Logo} alt="Logo" /></div>
+  </div>;
+}
+
+export default HeroSection;
 
